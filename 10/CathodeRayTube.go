61591c942403f457c787a9bba68914abd8dc0b5a
Mikhail Abramov <mabramov@redhat.com>
2022-12-11 18:41:45 +0100

    Day 10, add part two

--- 10/CathodeRayTube.go
+++ 10/CathodeRayTube.go
@@ -21,6 +21,18 @@ func incrementCycleCount(cyclesCount, X, sum *int) {
 	}
 }
 
+func printSprite(cyclesCount, X *int) {
+	iteration := (*cyclesCount)%40
+	if iteration==0 && *cyclesCount<=220{
+		fmt.Println()
+	}
+	if *X-1 == iteration || *X == iteration || *X+1 == iteration{
+		fmt.Print("#")
+	} else {
+		fmt.Print(".")
+	}
+}
+
 func main(){
 
 	cyclesCount := 0
@@ -37,16 +49,18 @@ func main(){
 
 	for fs.Scan() {
 		line := strings.Fields(fs.Text())
+		printSprite(&cyclesCount, &X)
 		incrementCycleCount(&cyclesCount, &X, &sum)
 		if len(line) == 2 {
 			addx, _ := strconv.Atoi(line[1])
+			printSprite(&cyclesCount, &X)
 			incrementCycleCount(&cyclesCount, &X, &sum)
 			X += addx
 		}
 
 	}
 	
-	fmt.Println("Cycles:", cyclesCount, "X:", X, "Sum:", sum)
+	fmt.Println("\nCycles:", cyclesCount, "X:", X, "Sum:", sum)
 
 	readFile.Close()
 }
